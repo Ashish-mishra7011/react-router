7421ac46bb5dec8491824dbd31e775d9b4a634aa
Ashish <am7011414408@gmail.com>
2024-03-03 23:32:41 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import User from "./components/User";
-import Github from "./components/Github";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Layout from './Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import User from './components/User';
+import Github from './components/Github';
+
 
 function App() {
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="github" element={<Github />} />
-            <Route path="user/:userid" element={<User />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    <BrowserRouter>
+    <Routes>
+    <Route path="" element={<Layout/>}>
+     <Route path="/" element={<Home/>} />
+     <Route path='about' element={<About/>} />
+   <Route path='contact' element={<Contact/>} />
+     <Route  path='github' element={<Github/>} />
+    <Route path='user/:userid' element={<User/>} />
+</Route>
+    </Routes>
+  </BrowserRouter>
+  </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 // import {Route,RouterProvider,createBrowserRouter} from 'react-router-dom'
@@ -65,5 +65,8 @@ import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+  <React.StrictMode>
+   
+   <App/>
+  </React.StrictMode>
 )
